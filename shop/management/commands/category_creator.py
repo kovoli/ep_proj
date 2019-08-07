dbf95211182207a567890d6818881b2366c4e1ed
imac <kovoli1985@gmail.com>
2019-08-07 12:32:03 +0200

    1

--- shop/management/commands/category_creator.py
+++ shop/management/commands/category_creator.py
@@ -4,10 +4,12 @@ from shop.models import Category
 from django.db import IntegrityError
 from unidecode import unidecode
 
+
 def open_cat_xml(file):
     tree = ET.parse(f'./shop/management/category_xml/{file}')
     get_root = tree.getroot()
     return get_root
+
 
 class Command(BaseCommand):
 
@@ -17,24 +19,26 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         root = open_cat_xml(kwargs['file_name'])
-        print(len(root.findall('.//category')))
+        count_created_cat = 0
         for cat in root.findall('.//category'):
             try:
                 category_name = cat.text
                 if 'parentId' not in cat.attrib:
                     Category.objects.get_or_create(name=category_name)
-
+                    count_created_cat += 1
                 else:
                     parent_id = cat.attrib['parentId']
                     parent_category = root.find(f".//category[@id='{parent_id}']").text
                     parent_category_instanz = Category.objects.get(name=parent_category)
                     Category.objects.get_or_create(name=category_name, parent=parent_category_instanz)
-                    self.stdout.write(self.style.SUCCESS(f'Подкатегория созданна: {category_name}'))
+                    count_created_cat += 1
             except IntegrityError:
                 Category.objects.get_or_create(name=category_name, parent=parent_category_instanz, slug=f'{unidecode(category_name.lower())}-{unidecode(parent_category.replace(" ", "-").lower())}')
-
+                count_created_cat += 1
             except Exception as a:
                 self.stdout.write(self.style.ERROR(a))
+        all_cat_count = len(root.findall('.//category'))
 
-        self.stdout.write(self.style.SUCCESS('Финиш'))
+        self.stdout.write(self.style.SUCCESS(f'Всего категории в XML: {all_cat_count}' ))
+        self.stdout.write(self.style.SUCCESS(f'Категорий созданно: {count_created_cat}'))
 
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from .models import Category
+
+
+
+# ---------- HELPER FUNCTIONS -----------------------
+def menu():
+    categories_nav = Category.objects.all()
+    return categories_nav
 
 
 def home_page(request):
-    return render(request, 'home_page.html')
+
+    return render(request, 'home_page.html', {'categories': menu()})
 
 
 def category_catalog(request, slug=None):
-    return render(request, 'shop/category_product_list.html')
+    return render(request, 'shop/category_product_list.html', {'categories': menu()})
 
