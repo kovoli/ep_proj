c23b6a20e6e27f9ca7cb3132ce15709874d94400
imac <kovoli1985@gmail.com>
2019-08-12 17:36:01 +0200

    1

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import QueryDict
 from .models import Product, Category, Vendor
 from .forms import CommentForm
 from django.db.models import Min, Max, Count
-#from . import helpers
+from . import helpers
 from watson import search as watson
 from django.db.models import F
+
 
 def home_page(request):
     favorites_cat = Category.objects.filter(favorites=True)
@@ -35,7 +37,6 @@ def product_detail(request, slug):
             new_comment.product = product
             new_comment.save()
             return redirect('shop:product_detail', slug=product.slug)
-
     else:
         comment_form = CommentForm()
 
@@ -49,5 +50,46 @@ def product_detail(request, slug):
 
 
 def category_catalog(request, slug=None):
-    return render(request, 'shop/category_product_list.html')
+    category = get_object_or_404(Category, slug=slug)
+    # -------- Крошки и их потомки
+    breadcrumbs = Category.get_ancestors(category, include_self=True)
+    # -------- Если уровень категории ниже первой выводятся только категории без товаров
+    if category.get_level() <= 1:
+        cat = category.get_descendants().order_by('tree_id', 'id', '-name')
+        return render(request, 'shop/category_catalog.html', {'category': category,
+                                                              'cat': cat,
+                                                              'breadcrumbs': breadcrumbs})
+
+    if category.get_level() >= 2:
+
+
+        list_pro = Product.objects.filter(category__in=Category.objects.get(id=category.id).get_descendants(include_self=True)).annotate(min_price=Min('prices__price')).order_by('-views')
+        vendors_ids = list_pro.values_list('vendor_id', flat=True).order_by().distinct()
+        vendors = Vendor.objects.filter(id__in=vendors_ids)
+        products_list = helpers.pg_records(request, list_pro, 30)
+
+        category = get_object_or_404(Category, slug=slug)
+        cat = category.get_descendants(include_self=True).order_by('tree_id', 'id', 'name')
+        last_node = category.get_siblings(include_self=True)
+
+
+
+        ven = request.GET.get('vendors_get')
+
+        print(type(ven))
+
+        all_param = {'van': ven}
+        qer = QueryDict.fromkeys(['van'], value=ven)
+        print(qer)
+        print(request.GET)
+
+        return render(request, 'shop/category_product_list.html', {'products_list': products_list,
+                                                                   'category': category,
+                                                                   'vendors': vendors,
+                                                                   'cat': cat,
+                                                                   'last_node': last_node,
+
+                                                                   'breadcrumbs': breadcrumbs,
+                                                                   #'filter_brand': filter_brand,
+                                                                   })
 
